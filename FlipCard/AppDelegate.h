c29c627b615684be7d05a3190aa35573545f0ec9
KIMSEONGTAN <kimseongtan@KIMSEONGTAN-no-MacBook-Air.local>
2014-07-24 00:23:31 +0900

    init

---
+++ FlipCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlipCard
+//
+//  Created by KIMSEONGTAN on 2014/07/24.
+//  Copyright (c) 2014年 growMerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlipCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlipCard
+//
+//  Created by KIMSEONGTAN on 2014/07/24.
+//  Copyright (c) 2014年 growMerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
